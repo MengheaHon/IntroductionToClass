17a3c8fb636ec512a33b249f68d870fb4bebfbb4
MENGHEA <MENGHEA@MENG-PC>
2022-03-11 20:52:05 +0900

    Add project files.

---
+++ IntroductionToClass/Person.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IntroductionToClass
+{
+    public class Person
+    {
+        // Contructor that takes no arguments:
+        public Person()
+        {
+            Name = "unknow";
+        }
+        // Contructor that takes one arguments:
+        public Person(string name)
+        {
+            Name = name;
+        }
+        // Auto-implement readonly property
+        public string Name { get; }
+        public override string ToString()
+        {
+            return Name;
+        }
+    }
+}
---
+++ IntroductionToClass/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace IntroductionToClass
+{
+    class TestPerson
+    {
+        static void Main(string[] args)
+        {
+            // Call the contructor that has no param
+            var person1 = new Person();
+            Console.WriteLine(person1.Name);
+            // Call the contructor that has one param
+            var person2 = new Person("John Doe");
+            Console.WriteLine(person2.Name);
+            // Get the string representation of the person2 instance.
+            Console.WriteLine(person2);
+        }
+    }
+}
+//output
+// unknow
+// John Doe
+// John Doe
